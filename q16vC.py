d8d5ecb93861513d20aec6cfd3bbbe24969b2176
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-06-28 21:11:31 +1000

    finished program! looking good

--- q16vC.py
+++ q16vC.py
@@ -16,40 +16,8 @@ JAVASCRIPT = Skill("JavaScript", ["JavaScript", "Javascript", "javascript", "JS"
 
 SKILL_LIST = [PYTHON, RUBY, BASH, GIT, HTML, TDD, CSS, JAVASCRIPT]
 
-welcome = "\n---ACME CODING SKILL TEST---\nWelcome to ACME Corporation's skill assessment tool. This program will evaluate your potential suitability for a developer role\nbased on which skills you have.\n"
-
-instructions = "\n---INSTRUCTIONS---\n\n1. Enter skill\n2. Press Enter key\n3. Repeat for all of your skills\n4. Once completed, enter 'X'"
-
-
-def print_message(section):
-    print(section)
-    next()
-
-# def get_name(name):
-#     name = input("What is your name?")
-
-def next():
-    answer = input("Are you ready to continue? (Y or N):  ")
-    if answer == "Y":
-        return True  
-
-def get_input(index):
-    user_input = input(f"Skill {index}: ")
-    return user_input
-
-def check_input():
-    done = "X"
-    index = 1
-    score = 0
-    while True:
-        input = get_input(index)
-        index += 1
-        if input == done:
-            break
-        for i in SKILL_LIST:
-            if input in i.name_variations:
-                score += i.weight
-    return score
+YES_VARIATIONS = ("Y", "Yes", "y", "YES", "yes")
+NO_VARIATIONS = ("N", "No", "n", "NO", "no")
 
 def calculate_max_score(list_of_skills):
     max_score = 0
@@ -58,19 +26,98 @@ def calculate_max_score(list_of_skills):
         max_score += value
     return max_score
 
+def next(prompt):
+    answer = ""
+    while True:
+        answer = input(f"\n{prompt}  ")
+        if answer in (YES_VARIATIONS):
+            return True
 
-def print_results(score, max_score):
+def get_input(index):
+    user_input = input(f"Skill {index}: ")
+    return user_input
+
+def check_skills():
+    done = "X"
+    index = 1
+    score = 0
+    remaining_skills = []
+    while True:
+        input = get_input(index)
+        index += 1
+        if input == done:
+            break
+        for i in SKILL_LIST:
+            if input in i.name_variations:
+                score += i.weight
+                SKILL_LIST.remove(i)
+    return score
+
+def get_percentage(score, max_score):
+    percentage = int((score / max_score) * 100)
+    return percentage
+
+def print_results(score, max_score, user_name):
     title = "\n---RESULTS---\n"
-    results = f"You scored {score} out of {max_score}"
-    print(title + results)
+    percentage = get_percentage(score, max_score)
+    results = f"You scored {percentage}% ({score} out of {max_score})\n"
+    message = evaluate_results(user_name, percentage)
+    print(results)
+
+def evaluate_results(name, percentage):
+    message = "your suitability for the role is"
+    print("\n---RESULTS---\n")
+    if percentage <= 33:
+        print(f"Sorry {name}, {message} low.")
+    elif percentage > 33 and percentage <=66:
+        print(f"Good job {name}, {message} average.")
+    elif percentage > 66 and percentage <= 90:
+        print(f"Well done {name}, {message} high.")
+    else:
+        print(f"Congratulations {name}, you're hired!")
+
+def give_feedback(unmet_criteria):
+    title = "\n---RECOMMENDED LEARNING---\n"
+    print(f"{title}\nTo meet the full requirements for this role, you must also learn these skills:")
+    for i in unmet_criteria:
+        if i.weight < 2:
+            print(f"\n--{i.skill_name}--\nIf you learn {i.skill_name} you will improve your score by {i.weight} point")
+        else:
+            print(f"\n--{i.skill_name}--\nIf you learn {i.skill_name} you will improve your score by {i.weight} points")
+    print("\n")
+    
 
 def main_program():
+    #CALCULATE MAX SCORE OF TEST#
     max_score = calculate_max_score(SKILL_LIST)
-    print_message(welcome)
-    print_message(instructions)
-    score = check_input()
-    results = print_results(score, max_score)
-
+    #MESSAGES#
+    intro = "\n---ACME CODING SKILL TEST---\n\nWelcome to ACME Corporation's skill assessment tool. This program will evaluate your potential suitability for a developer role\nbased on which skills you have.\n"
+    instructions = "\n---INSTRUCTIONS---\n\n1. Enter skill\n2. Press Enter key\n3. Repeat for all of your skills\n4. Once completed, enter 'X'"
+    next_continue = "Are you ready to continue?"
+    #PRINT WELCOME MESSAGE#
+    print(intro)
+    #TAKE NAME#
+    user_name = input("Please enter your name:  ")
+    #WELCOME THEM WITH THEIR NAME#
+    welcome = "\nWelcome {user_name}!"
+    #PRINT INSTRUCTIONS#
+    print(instructions)
+    #IF USER WANTS TO PROCEED#
+    if next(next_continue):
+        goodbye = f"\n---GOODBYE---\n\nThank you for choosing to use the ACME Code Skill Test program, {user_name}!\nIt is a tremendously helpful tool that benefits both the applicant and recruitment agent with a system that evaluates a candidate's role suitability based on their experience with the particular skills a role needs as outlined by the job description's criteria.\n\n---END PROGRAM---\n"
+        score = check_skills()
+        percentage = get_percentage(score, max_score)
+        results = print_results(score, max_score, user_name)
+        if percentage < 100:
+            see_feedback = input("Would you like to see how you can improve your score? ")
+            if see_feedback in NO_VARIATIONS:
+                print(goodbye)
+            elif see_feedback in YES_VARIATIONS:
+                feedback_message = give_feedback(SKILL_LIST)
+                if next(next_continue):
+                    print(goodbye)
+            
+        
 
 main_program()
 
