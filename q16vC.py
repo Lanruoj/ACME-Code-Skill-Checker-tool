66c32104b7a9005f487fa7cb7561d38b67a8c1ec
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-06-28 14:36:51 +1000

    added Skill class and skill objects

--- q16vC.py
+++ q16vC.py
@@ -1,1 +1,23 @@
-hello
+
+class Skill:
+    def __init__(self, skill_name, name_variations, weight):
+        self.skill_name = skill_name
+        self.name_variations = name_variations
+        self.weight = weight
+
+PYTHON = Skill("Python", ["Python", "python"], 1)
+RUBY = Skill("Ruby", ["Ruby", "ruby"], 2)
+BASH = Skill("Bash", ["Bash", "bash"], 4)
+GIT = Skill("Git", ["Git", "git"], 8)
+HTML = Skill("HTML", ["HTML", "Html", "html"], 16)
+TDD = Skill("TDD", ["TDD", "Tdd", "tdd"], 32)
+CSS = Skill("CSS", ["CSS", "Css", "css"], 64)
+JAVASCRIPT = Skill("JavaScript", ["JavaScript", "Javascript", "javascript", "JS", "js"], 128)
+
+
+
+
+
+
+
+
