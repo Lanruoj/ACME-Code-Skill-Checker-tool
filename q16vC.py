e7ec04562669972110963e74fb3334047387f895
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-06-28 16:06:07 +1000

    get input, check input, calc max score, print results, NO MAIN

--- q16vC.py
+++ q16vC.py
@@ -14,6 +14,47 @@ TDD = Skill("TDD", ["TDD", "Tdd", "tdd"], 32)
 CSS = Skill("CSS", ["CSS", "Css", "css"], 64)
 JAVASCRIPT = Skill("JavaScript", ["JavaScript", "Javascript", "javascript", "JS", "js"], 128)
 
+SKILL_LIST = [PYTHON, RUBY, BASH, GIT, HTML, TDD, CSS, JAVASCRIPT]
+
+
+
+def get_input(index):
+    user_input = input(f"Skill {index}: ")
+    return user_input
+
+def check_input():
+    # user_input_list = []
+    done = "X"
+    index = 1
+    score = 0
+    while True:
+        input = get_input(index)
+        index += 1
+        if input == done:
+            break
+        for i in SKILL_LIST:
+            if input in i.name_variations:
+                score += i.weight
+                print(score)
+    return score
+
+def calculate_max_score(list_of_skills):
+    max_score = 0
+    for i in list_of_skills:
+        value = i.weight
+        max_score += value
+    return max_score
+
+
+def print_results(score, max_score):
+    # max_score = calculate_max_score(SKILL_LIST)
+    # score = check_input()
+    results = f"You scored {score} out of {max_score}"
+    print(results)
+
+
+print_results(2, 255)
+
 
 
 
